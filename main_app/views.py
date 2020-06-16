f988891b42fde5a087c3a328f7351d06337b589d
fletcherj17 <fletcherejones@gmail.com>
2020-06-15 21:22:18 -0400

    added view page

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Dog
 
 # Create your views here.
 
@@ -12,21 +13,12 @@ def test(request):
 def about(request):
     return render(request, 'about.html')
 
-
-# Add the Dog class & list and view function below the imports
-class Dog:  # Note that parens are optional if not inheriting from another class
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-dogs = [
-Dog('Lolo', 'tabby', 'foul little demon', 3),
-Dog('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-Dog('Raven', 'black tripod', '3 legged dog', 4)
-]
-
 def dogs_index(request):
+    dogs = Dog.objects.all()
     context = {'dogs': dogs}
     return render(request, 'dogs/index.html', context)
+
+def details (request, dog_id):
+    dog = Dog.objects.get(id = dog_id)
+    context = {'dog': dog}
+    return render(request, 'dogs/details.html', context)
